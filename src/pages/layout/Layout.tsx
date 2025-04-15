071dc5ff1a8655868839f1e3b819c8bb16c880b7
Francisco Bermudez <bermudezfran15@gmail.com>
2025-04-15 20:07:10 -0300

    add animation

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,15 +1,31 @@
-import '../../styled-components/layout.module.css';
-
+import { useRef } from 'react';
+import { Outlet, useLocation } from "react-router-dom";
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import { Navbar } from "../../components/header/Navbar";
 import { Footer } from "../../components/footer/Footer";
-import { Outlet } from "react-router-dom";
+import '../../styled-components/layout.module.css';
 
 export const Layout = () => {
+  const location = useLocation();
+  const nodeRef = useRef(null); // 👈 importante
+
   return (
     <div className="layout">
       <Navbar />
       <main className="content">
-        <Outlet />
+        <SwitchTransition>
+          <CSSTransition
+            key={location.pathname}
+            nodeRef={nodeRef} // 👈 pasamos el ref acá
+            timeout={300}
+            classNames="fade"
+            unmountOnExit
+          >
+            <div ref={nodeRef} className="route-wrapper">
+              <Outlet />
+            </div>
+          </CSSTransition>
+        </SwitchTransition>
       </main>
       <Footer />
     </div>
